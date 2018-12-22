06742c9cb7d67569ae8a79f2c1c205412cb817be
andrej <andrej.borstnik@matheo.si>
2018-12-22 23:02:20 +0100

    Courses on map. They open the maps app

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,7 +1,21 @@
-import {StyleSheet, Text, View, TouchableOpacity} from "react-native";
+import {StyleSheet, Text, View, TouchableOpacity, Image, Dimensions, Button, Platform} from "react-native";
 import React from "react";
 
 
+let isIPhoneX = function () {
+    // iPhone X & iPhone XS, iPhone XR & iPhone XS Max
+    let X_WIDTH = [375, 414];
+    let X_HEIGHT = [812, 896];
+
+    let D_HEIGHT = Dimensions.get('window').height
+    let D_WIDTH = Dimensions.get('window').width
+    if (Platform.OS !== 'ios') return false;
+    for (let i = 0; i < X_WIDTH.length; i++) {
+        if (D_HEIGHT === X_HEIGHT[i] && D_WIDTH === X_WIDTH[i]
+            || D_HEIGHT === X_WIDTH[i] && D_WIDTH === X_HEIGHT[i]) return true;
+    }
+    return false;
+}
 
 
 
@@ -19,27 +33,55 @@ export default class HomeScreen extends React.Component {
     }
 
     render() {
+        let mapImage = require('../assets/map.png')
+        let screenWidth = Dimensions.get('window').width
         return (
             <View
                 style={styles.container}
             >
+                <Text
+                    style={{
+                        fontSize: 30,
+                        margin: 20,
+                    }}
+                >
+                    Poišči Proge
+                </Text>
                 <TouchableOpacity
                     onPress={this.navigateToMap}
                 >
-                    <Text style={styles.welcome}>Maps</Text>
+                    <Image
+                        source={mapImage}
+                        resizeMode={'cover'}
+                        style={{
+                            width: screenWidth,
+                            height: 200,
+                        }}
+                    />
                 </TouchableOpacity>
 
-                <TouchableOpacity
-                    onPress={this.navigateToCourse}
-                >
-                    <Text style={styles.welcome}>Location test</Text>
-                </TouchableOpacity>
+                {/*<TouchableOpacity*/}
+                    {/*onPress={this.navigateToCourse}*/}
+                {/*>*/}
+                    {/*<Text style={styles.welcome}>Location test</Text>*/}
+                {/*</TouchableOpacity>*/}
 
-                <TouchableOpacity
-                    onPress={this.navigateToQR}
+                <View
+                    style={{
+                        paddingHorizontal: Platform.OS === 'ios' ? 0 : 24,
+                        paddingVertical: 8,
+                        position: 'absolute',
+                        bottom: isIPhoneX() ? 20 + 34 : 20,
+                        width: screenWidth,
+                        // backgroundColor: Platform.OS === 'ios' ? '#fff' : 'transparent'
+                    }}
                 >
-                    <Text style={styles.welcome}>QR Test</Text>
-                </TouchableOpacity>
+                    <Button
+                        onPress={this.navigateToQR}
+                        title="Začni Tek"
+                        color="#4A90E2"
+                    />
+                </View>
             </View>
         );
     }
@@ -48,9 +90,9 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // backgroundColor: '#F5FCFF',
     },
     welcome: {
         fontSize: 20,
--- pages/MapView.js
+++ pages/MapView.js
@@ -1,44 +1,98 @@
 import React from 'react';
-import {StyleSheet, View, Dimensions, Text} from "react-native";
+import {StyleSheet, View, Dimensions, Text, Linking, Platform} from "react-native";
 
-import MapView, { Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
-
+import MapView, {Polyline, Marker} from 'react-native-maps';
+import _ from 'lodash'
 
 
 export default class MyMapView extends React.Component {
-    render () {
+
+    courses = [
+        {
+            id: '1',
+            name: 'Proga 1',
+            coordinates: {
+                latitude: 46.11,
+                longitude: 14.6,
+            }
+        },
+        {
+            id: '2',
+            name: 'Proga 2',
+            coordinates: {
+                latitude: 46.31,
+                longitude: 14,
+            }
+        },
+        {
+            id: '3',
+            name: 'Proga 3',
+            coordinates: {
+                latitude: 46.17,
+                longitude: 14.1,
+            }
+        },
+    ]
+
+    selectCourse = (e) => {
+        const scheme = Platform.select({ios: 'maps:0,0?q=', android: 'geo:0,0?q='});
+        const latLng = `${e.nativeEvent.coordinate.latitude},${e.nativeEvent.coordinate.longitude}`;
+        const label = e.nativeEvent.id;
+        const url = Platform.select({
+            ios: `${scheme}${label}@${latLng}`,
+            android: `${scheme}${latLng}(${label})`
+        });
+        Linking.openURL(url)
+    }
+
+    render() {
         return (
             <View style={styles.container}>
                 <Text>Test</Text>
                 <MapView
                     style={styles.map}
                     region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.015,
-                        longitudeDelta: 0.0121,
+                        latitude: 45.5,
+                        longitude: 15,
+                        latitudeDelta: 1,
+                        longitudeDelta: 4,
                     }}
+                    maximumZ={10}
                 >
-                    <Polyline
-                        coordinates={[
-                            {latitude: 37.8025259, longitude: -122.4351431},
-                            {latitude: 37.7896386, longitude: -122.421646},
-                            {latitude: 37.7665248, longitude: -122.4161628},
-                            {latitude: 37.7734153, longitude: -122.4577787},
-                            {latitude: 37.7948605, longitude: -122.4596065},
-                            {latitude: 37.8025259, longitude: -122.4351431}
-                        ]}
-                        strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
-                        strokeColors={[
-                            '#7F0000',
-                            '#00000000', // no color, creates a "long" gradient between the previous and next coordinate
-                            '#B24112',
-                            '#E5845C',
-                            '#238C23',
-                            '#7F0000'
-                        ]}
-                        strokeWidth={6}
-                    />
+
+                    {
+                        _.map(this.courses, (course) => {
+                            if (!course) return null
+                            return <Marker
+                                key={'course' + course.id}
+                                coordinate={course.coordinates}
+                                pinColor={'green'}
+                                onPress={this.selectCourse}
+                                identifier={course.name}
+                            />
+                        })
+                    }
+
+                    {/*<Polyline*/}
+                    {/*coordinates={[*/}
+                    {/*{latitude: 37.8025259, longitude: -122.4351431},*/}
+                    {/*{latitude: 37.7896386, longitude: -122.421646},*/}
+                    {/*{latitude: 37.7665248, longitude: -122.4161628},*/}
+                    {/*{latitude: 37.7734153, longitude: -122.4577787},*/}
+                    {/*{latitude: 37.7948605, longitude: -122.4596065},*/}
+                    {/*{latitude: 37.8025259, longitude: -122.4351431}*/}
+                    {/*]}*/}
+                    {/*strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider*/}
+                    {/*strokeColors={[*/}
+                    {/*'#7F0000',*/}
+                    {/*'#00000000', // no color, creates a "long" gradient between the previous and next coordinate*/}
+                    {/*'#B24112',*/}
+                    {/*'#E5845C',*/}
+                    {/*'#238C23',*/}
+                    {/*'#7F0000'*/}
+                    {/*]}*/}
+                    {/*strokeWidth={6}*/}
+                    {/*/>*/}
                 </MapView>
             </View>
         );
